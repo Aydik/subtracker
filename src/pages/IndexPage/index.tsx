a462fdb44a65dc398f00a90696a901c50aec407d
Aydik <Nekkisuper@gmail.com>
2025-10-16 22:34:17 +0300

    feat(IndexPage): Адоптивный IndexPage

--- src/pages/IndexPage/index.tsx
+++ src/pages/IndexPage/index.tsx
@@ -1,7 +1,99 @@
 import type { FC } from 'react';
+import styles from './index.module.scss';
+import Marquee from 'react-fast-marquee';
+import { Button } from 'antd';
+import { Icon } from '@shared/ui/Icon';
+import { useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
+
+const services = [
+  ['spotify', 'apple-music', 'netflix', 'instagram', 'vk'],
+  ['yandex-music', 'kinopoisk', 'ivi', 'twitter', 'youtube'],
+  ['duolingo', 'telegram', 'roblox', 'wechat', 'mts'],
+];
 
 const IndexPage: FC = () => {
-  return <div>IndexPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.indexPage}>
+      <div className={styles.logos}>
+        {services.map((line, index) => (
+          <Marquee
+            className={styles.line}
+            key={index}
+            speed={20 + (2 - index) * 10 - (index % 2) * 10}
+            direction={index % 2 === 0 ? 'left' : 'right'}
+          >
+            <div className={styles.line}>
+              {Array(5).fill(
+                line.map((service) => (
+                  <img
+                    className={styles.logo}
+                    key={service}
+                    src={`public/assets/images/services/${service}.png`}
+                    alt={service}
+                  />
+                )),
+              )}
+            </div>
+          </Marquee>
+        ))}
+      </div>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <h1>SubTracker</h1>
+          <h2>
+            Умный контроль ваших <br /> платежей
+          </h2>
+        </div>
+        <div className={styles.actions}>
+          <div className={styles.auth}>
+            <Button
+              key="register"
+              onClick={() => navigate('/auth/register')}
+              type="default"
+              size="large"
+            >
+              Создать аккаунт
+            </Button>
+            <Button key="login" onClick={() => navigate('/auth/login')} type="primary" size="large">
+              Войти
+            </Button>
+          </div>
+          <div className={styles.divider}>
+            <div className={styles.divider__line} />
+            <span className={styles.divider__text}>или</span>
+            <div className={styles.divider__line} />
+          </div>
+          <div className={styles.oAuth}>
+            <Button
+              key="google"
+              className={styles.oAuth__button}
+              ghost
+              shape="circle"
+              icon={<Icon name="google" />}
+            />
+            <Button
+              key="yandex"
+              className={styles.oAuth__button}
+              ghost
+              shape="circle"
+              icon={<Icon name="yandex" />}
+            />
+            <Button
+              key="vk"
+              className={styles.oAuth__button}
+              ghost
+              shape="circle"
+              icon={<Icon name="vk" />}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={clsx(styles.border, styles.border_left)} />
+      <div className={clsx(styles.border, styles.border_right)} />
+    </div>
+  );
 };
 
 export default IndexPage;
--- src/shared/theme/config/themeConfig.ts
+++ src/shared/theme/config/themeConfig.ts
@@ -1,10 +1,33 @@
 import type { ThemeConfig } from 'antd';
 
-export const lightTheme: ThemeConfig = {
+export const darkTheme: ThemeConfig = {
   token: {
     colorPrimary: '#f8d81c',
     fontFamily: 'Tektur, sans-serif',
+    colorSplit: '#ffffff',
+  },
+  components: {
+    Button: {
+      // default
+      defaultBg: '#2c3844',
+      defaultColor: '#ffffff',
+      defaultBorderColor: 'transparent',
+
+      defaultActiveBg: '#3a4a58',
+      defaultHoverBg: '#3a4a58',
+      defaultHoverColor: '#ffffff',
+      defaultHoverBorderColor: '#ffffff',
+
+      // primary
+      colorPrimaryBgHover: '#f8d93e',
+      primaryColor: '#000000',
+
+      // sizes
+      fontWeight: 500,
+      paddingInlineLG: 20,
+      paddingBlockLG: 15,
+    },
   },
 };
 
-export const darkTheme: ThemeConfig = lightTheme;
+export const lightTheme: ThemeConfig = darkTheme;
