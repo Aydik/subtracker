e34801e7c2dcd7ad5c5a70c73e3404f298960b27
ArtamonovaAngelina <vdc040918@gmail.com>
2026-04-24 00:11:22 +0300

    feat(refactor): переименование компонентов

--- src/shared/ui/PageLoader/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react';
-import { Spin } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
-
-export const PageLoader: FC = () => {
-  return (
-    <Spin fullscreen size="large" tip="Загрузка страницы" indicator={<LoadingOutlined spin />} />
-  );
-};
