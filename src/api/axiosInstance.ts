b7cab13309d47a77fa012864953810894442fdce
Aydik <Nekkisuper@gmail.com>
2026-05-23 19:53:41 +0300

    deploy: поправлены assets

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 import type { AxiosRequestConfig } from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || '/';
+
 export const axiosInstance = axios.create({
-  baseURL: '/',
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
